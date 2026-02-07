85fdc0d1fadd9f8647020589077b41e3edf9afae
bunni18 <fgcommunity1@gmail.com>
2026-02-06 20:57:12 -0500

    Update public-services-summary.sql

--- SQL/public-services-summary.sql
+++ SQL/public-services-summary.sql
@@ -21,6 +21,16 @@ VALUES
 ('Housing Assistance'),
 ('Food Assistance'),
 ('Workforce Training');
+SELECT
+  p.program_name,
+  COUNT(e.enrollment_id) AS total_enrolled,
+  FLOOR(AVG(e.benefit_amount)) AS avg_benefit_amount,
+  SUM(e.benefit_amount) AS total_benefits
+FROM enrollments e
+JOIN programs p
+  ON e.program_id = p.program_id
+GROUP BY p.program_name;
+
 
 INSERT INTO citizens (full_name, date_of_birth)
 VALUES
