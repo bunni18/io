1ad97273a0b64910287b7479fd38d257ca1031a5
bunni18 <fgcommunity1@gmail.com>
2026-02-06 20:20:28 -0500

    Update public-services-summary.sql

--- SQL/public-services-summary.sql
+++ SQL/public-services-summary.sql
@@ -1,1 +1,8 @@
+SELECT
+  p.program_name,
+  COUNT(*) AS total_enrolled,
+  FLOOR(AVG(e.benefit_amount)) AS avg_benefit
+FROM enrollments e
+JOIN programs p ON e.program_id = p.program_id
+GROUP BY p.program_name;
 
