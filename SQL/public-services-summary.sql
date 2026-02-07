be1d776e99607f2d7c46463e4daf811ac589c34f
bunni18 <fgcommunity1@gmail.com>
2026-02-06 21:15:29 -0500

    Delete SQL/public-services-summary.sql

--- SQL/public-services-summary.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  p.program_name,
-  COUNT (*) AS total_enrolled,
-  FLOOR (AVG(e.benefit_amount)) AS avg_benefit
-FROM enrollments e
-JOIN programs p on e.program_id = p.program_id
-GROUP BY p.program_name;
